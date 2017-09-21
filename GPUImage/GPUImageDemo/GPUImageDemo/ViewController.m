884c66698c5c0e4deee50b383bfa971ac003d595
wangzhen <869158130@qq.com>
2017-09-21 14:39:13 +0800

    GPUImage效果

---
+++ GPUImage/GPUImageDemo/GPUImageDemo/ViewController.m
@@ -0,0 +1,171 @@
+//
+//  ViewController.m
+//  GPUImageDemo
+//
+//  Created by wangzhen on 2017/9/21.
+//  Copyright © 2017年 wz. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GPUImage.h"
+
+@interface ViewController ()<UIImagePickerControllerDelegate ,UINavigationControllerDelegate, UIActionSheetDelegate>
+{
+    UIImage *originalImage;
+}
+@property(nonatomic, weak) IBOutlet UIImageView *selectedImageView;
+@property(nonatomic, weak) IBOutlet UIBarButtonItem *filterButton;
+@property(nonatomic, weak) IBOutlet UIBarButtonItem *saveButton;
+
+- (IBAction)photoFromAlbum;
+- (IBAction)photoFromCamera;
+- (IBAction)applyImageFilter:(id)sender;
+- (IBAction)saveImageToAlbum;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)photoFromAlbum
+{
+    UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
+    photoPicker.delegate = self;
+    photoPicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:photoPicker animated:YES completion:NULL];
+    
+}
+
+- (IBAction)photoFromCamera
+{
+    UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
+    
+    photoPicker.delegate = self;
+    photoPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:photoPicker animated:YES completion:NULL];
+    
+}
+
+- (IBAction)applyImageFilter:(id)sender
+{
+    UIActionSheet *filterActionSheet = [[UIActionSheet alloc] initWithTitle:@"Select Filter"
+                                                                   delegate:self
+                                                          cancelButtonTitle:@"Cancel"
+                                                     destructiveButtonTitle:nil
+                                                          otherButtonTitles:@"Grayscale", @"Sepia", @"Sketch", @"Pixellate", @"Color Invert", @"Toon", @"Pinch Distort", @"None", nil];
+    
+    [filterActionSheet showFromBarButtonItem:sender animated:YES];
+}
+
+- (IBAction)saveImageToAlbum
+{
+    UIImageWriteToSavedPhotosAlbum(self.selectedImageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    NSString *alertTitle;
+    NSString *alertMessage;
+    
+    if(!error)
+    {
+        alertTitle   = @"Image Saved";
+        alertMessage = @"Image saved to photo album successfully.";
+    }
+    else
+    {
+        alertTitle   = @"Error";
+        alertMessage = @"Unable to save to photo album.";
+    }
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:alertTitle
+                                                    message:alertMessage
+                                                   delegate:self
+                                          cancelButtonTitle:@"Okay"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)photoPicker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.saveButton.enabled = YES;
+    self.filterButton.enabled = YES;
+    
+    originalImage = [info valueForKey:UIImagePickerControllerOriginalImage];
+    
+    [self.selectedImageView setImage:originalImage];
+    
+    [photoPicker dismissModalViewControllerAnimated:YES];
+}
+
+#pragma mark - UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    GPUImageFilter *selectedFilter;
+    
+    switch (buttonIndex) {
+        case 0:
+            selectedFilter = [[GPUImageGrayscaleFilter alloc] init];
+            break;
+        case 1:
+            selectedFilter = [[GPUImageSepiaFilter alloc] init];
+            break;
+        case 2:
+            selectedFilter = [[GPUImageSketchFilter alloc] init];
+            break;
+        case 3:
+            selectedFilter = [[GPUImagePixellateFilter alloc] init];
+            break;
+        case 4:
+            selectedFilter = [[GPUImageColorInvertFilter alloc] init];
+            break;
+        case 5:
+            selectedFilter = [[GPUImageToonFilter alloc] init];
+            break;
+        case 6:
+            selectedFilter = [[GPUImagePinchDistortionFilter alloc] init];
+            break;
+        case 7:
+            selectedFilter = [[GPUImageFilter alloc] init];
+            break;
+        default:
+            break;
+    }
+    
+    UIImage *filteredImage = [selectedFilter imageByFilteringImage:originalImage];
+    [self.selectedImageView setImage:filteredImage];
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GPUImage/README.md
@@ -0,0 +1,19 @@
+# # GPUImage
+
+
+
+GPUImage同样可以用来实现各种各样的效果
+
+基础基础可参考[Build a Photo App with GPUImage](https://code.tutsplus.com/tutorials/build-a-photo-app-with-gpuimage--mobile-12223)
+
+要注意的是，在集成GPUImage时，在`Build Settings -> Header Search Paths `中路径要选择`recursive`，否则会提示找不到`import "GPUImage.h"`
+
+
+
+如下的分别是原图、应用`GPUImageGrayscaleFilter`后的效果、应用`GPUImageSepiaFilter`后的效果
+
+
+
+
+
+
